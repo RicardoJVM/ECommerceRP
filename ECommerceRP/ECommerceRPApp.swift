6a8c494b952cb3a023631007d3e99f99c2b87da2
Ricardo Valencia <rvalencia@t2company.com.uy>
2026-09-18 10:46:03 -0300

    Initial Commit

---
+++ ECommerceRP/ECommerceRPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceRPApp.swift
+//  ECommerceRP
+//
+//  Created by Ricardo Valencia on 18/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceRPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
